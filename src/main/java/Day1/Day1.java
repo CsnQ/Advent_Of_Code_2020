5b2a30f9e50c8f435aeea7264f6b0a97ecd188e6
CsnQ <c.quilley@gmail.com>
2020-12-01 11:58:12 +0000

    day1, part2 solution

--- src/main/java/Day1/Day1.java
+++ src/main/java/Day1/Day1.java
@@ -3,13 +3,29 @@ package Day1;
 public class Day1 {
     public static int findSum(int expectedTotal, int[] listOfNums){
         int arrayLength = listOfNums.length;
-        int firstNum = 0;
         int total=0;
         for (int i = 0; i < arrayLength; i++) {
             for (int j = 0; j < arrayLength; j++) {
                 if (listOfNums[i]+listOfNums[j]==expectedTotal){
                     total =  listOfNums[i]*listOfNums[j];
                 }
+            }
+        }
+
+        return total;
+    }
+
+    public static int findSumOfThree(int expectedTotal, int[] listOfNums){
+        int arrayLength = listOfNums.length;
+        int total=0;
+        for (int i = 0; i < arrayLength; i++) {
+            for (int j = 0; j < arrayLength; j++) {
+                for (int k = 0; k < arrayLength; k++) {
+                    if (listOfNums[i]+listOfNums[j]+listOfNums[k]==expectedTotal){
+                        total = listOfNums[i]*listOfNums[j]*listOfNums[k];
+                    }
+                }
+
             }
         }
 
