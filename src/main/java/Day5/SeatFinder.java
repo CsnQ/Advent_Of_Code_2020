1d337f945a500bffc1c45a556f0fe0a85508669a
CsnQ <c.quilley@gmail.com>
2020-12-08 13:01:11 +0000

    completed day5, pt1

--- src/main/java/Day5/SeatFinder.java
+++ src/main/java/Day5/SeatFinder.java
@@ -2,39 +2,86 @@ package Day5;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SeatFinder {
     //128 seats
     //10 characters
     int numOfRows = 128;
     int numOfSeats = 8;
+    List<Integer> allRows;
+    List<Integer> allSeats;
     String input;
     String rowData;
     String seatData;
     List<Integer> rowInput;
     List<Integer> seatInput;
+    int myRow;
+    int mySeat;
 
 
     public SeatFinder(String input) {
         this.input = input;
         this.seatData = input.substring(input.length() - 3);
         this.rowData = input.substring(0, 7);
-        this.rowInput = transformData(rowData,'F','B' );
-        this.seatInput = transformData(seatData, 'L','R');
+        this.rowInput = transformData(rowData, 'F', 'B');
+        this.seatInput = transformData(seatData, 'L', 'R');
+        this.allRows = populateAllSeatsOrRows(numOfRows);
+        this.allSeats = populateAllSeatsOrRows(numOfSeats);
+        this.myRow = getMyRowOrSeat(this.rowInput,this.allRows);
+        this.mySeat = getMyRowOrSeat(this.seatInput,this.allSeats);
     }
 
     public List<Integer> transformData(String data, char firstLetter, char secondLetter) {
         List<Integer> input = new ArrayList<>();
         for (int i = 0; i < data.length(); i++) {
-            if (data.charAt(i)==firstLetter){
+            if (data.charAt(i) == firstLetter) {
                 input.add(0);
-            }else if (data.charAt(i)==secondLetter){
+            } else if (data.charAt(i) == secondLetter) {
                 input.add(1);
             }
         }
 
         return input;
+    }
+
+    public List<Integer> populateAllSeatsOrRows(int numOfSeatsOrRows) {
+        List<Integer> data = new ArrayList<>();
+        for (int i = 0; i < numOfSeatsOrRows; i++) {
+            data.add(i);
+        }
+
+        return data;
+    }
+
+    //FBFBBFFRLR
+//    Start by considering the whole range, rows 0 through 127.
+//    F means to take the lower half, keeping rows 0 through 63.
+//    B means to take the upper half, keeping rows 32 through 63.
+//    F means to take the lower half, keeping rows 32 through 47.
+//    B means to take the upper half, keeping rows 40 through 47.
+//    B keeps rows 44 through 47.
+//    F keeps rows 44 through 45.
+//    The final F keeps the lower of the two, row 44.
+    public int getMyRowOrSeat(List<Integer> convertedInstructions,List <Integer> rowOrSeatData) {
+        List<Integer> reducedList;
+        List<Integer> rows = rowOrSeatData;
+        while (rows.size()!=1){
+        for (int i = 0; i < convertedInstructions.size(); i++) {
+            switch (convertedInstructions.get(i)){
+                case(0):
+                    reducedList=rows.subList(0,rows.size()/2);
+                    rows = reducedList;
+                    break;
+
+                case(1):
+                    reducedList = rows.subList(rows.size()/2,rows.size());
+                    rows=reducedList;
+                    break;
+            }
+        }
+        }
+        return rows.get(0);
+
     }
 
     public String getRowData() {
@@ -51,6 +98,22 @@ public class SeatFinder {
 
     public List<Integer> getSeatInput() {
         return seatInput;
+    }
+
+    public List<Integer> getAllRows() {
+        return allRows;
+    }
+
+    public List<Integer> getAllSeats() {
+        return allSeats;
+    }
+
+    public int getMyRow() {
+        return myRow;
+    }
+
+    public int getMySeat() {
+        return mySeat;
     }
 
     @Override
--- src/test/java/Day5/SeatFinderTest.java
+++ src/test/java/Day5/SeatFinderTest.java
@@ -1,6 +1,11 @@
 package Day5;
 
+import Utilities.ReadFile;
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -12,7 +17,54 @@ public class SeatFinderTest extends TestCase {
         assertEquals("FBFBBFF", sf.getRowData());
         assertEquals("RLR", sf.getSeatData());
         assertEquals("SeatFinder{rowInput=[0, 1, 0, 1, 1, 0, 0], seatInput=[1, 0, 1]}", sf.toString());
+        assertEquals(128, sf.getAllRows().size());
+        assertEquals(8, sf.getAllSeats().size());
     }
 
+
+    public void testSeatFinderWithOneBitOfData() {
+        String data = "FBFBBFFRLR";
+        SeatFinder sf = new SeatFinder(data);
+        int resultForRow = sf.getMyRow();
+        int resultForSeat = sf.getMySeat();
+        int answer = resultForRow * 8 + resultForSeat;
+        assertEquals(44, resultForRow);
+        assertEquals(5, resultForSeat);
+        assertEquals(357, answer);
+    }
+
+    public void testSeatFinderGetAnswerForTestData() {
+        List<String> data = ReadFile.readLinesFromFile("src/main/java/Day5/Day5TestInput.txt");
+        List<Integer> answers = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            SeatFinder sf = new SeatFinder(data.get(i));
+            int resultForRow = sf.getMyRow();
+            int resultForSeat = sf.getMySeat();
+            int answer = resultForRow * 8 + resultForSeat;
+            answers.add(answer);
+        }
+        int maxSeatId = Collections.max(answers);
+        System.out.println(maxSeatId);
+        assertTrue(answers.get(0)==567);
+        assertTrue(answers.get(1)==119);
+        assertTrue(answers.get(2)==820);
+    }
+
+
+
+    public void testSeatFinderGetAnswerForRealData() {
+        List<String> data = ReadFile.readLinesFromFile("src/main/java/Day5/Day5Input.txt");
+        List<Integer> answers = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            SeatFinder sf = new SeatFinder(data.get(i));
+            int resultForRow = sf.getMyRow();
+            int resultForSeat = sf.getMySeat();
+            int answer = resultForRow * 8 + resultForSeat;
+            answers.add(answer);
+        }
+        int maxSeatId = Collections.max(answers);
+        System.out.println(maxSeatId);
+
+    }
 
 }
